1cabe39c13bfdb8086734f8c7199822e460fe253
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-03-06 16:05:46 -0500

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import './App.css';
 
-function App() {
-  return <div className='App'>hello</div>;
-}
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div id='main'>
+        <h1>Bill Keeper</h1>
+      </div>
+    </ApolloProvider>
+  );
+};
 
 export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import { AUTH_TOKEN } from '../constants';
+
+const Login = () => {
+  const [login, setLogin] = useState(true);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  return (
+    <div>
+      <h4 className='login-header'>{login ? 'Login' : 'Register'}</h4>
+      {!login && (
+        <input
+          value={name}
+          onChange={e => setName(e)}
+          type='text'
+          placeholder='Name'
+        />
+      )}
+      <input
+        value={username}
+        onChange={e => setUsername(e)}
+        type='text'
+        placeholder='Username'
+      />
+      <input
+        value={password}
+        onChange={e => setPassword(e)}
+        type='password'
+        placeholder='Password'
+      />
+      <button className='confirm-button' onClick={() => this._confirm}>
+        {login ? 'Login' : 'Register'}
+      </button>
+      <div className='switch-button' onClick={setLogin(!login)}>
+        {login ? 'I need to create an account.' : 'I already have an account.'}
+      </div>
+    </div>
+  );
+
+  _confirm = async () => {};
+
+  _saveUserData = token => {
+    localStorage.setItem(AUTH_TOKEN, token);
+  };
+};
+
+export default Login;
