ef336940e947a709a0a9e8b328689e98f925349c
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-03-16 20:37:05 -0400

    bills mapped to table

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,30 +6,43 @@ import { AUTH_TOKEN } from '../constants';
 const Home = props => {
   const authToken = localStorage.getItem(AUTH_TOKEN);
 
-  console.log(props.history.location.state.loginUser);
+  const { username, uname, bills } = props.history.location.state.loginUser;
 
   return (
     <div className='home-wrapper'>
       <div className='navbar'>
-        <div className='login-logout'>
-          {authToken ? (
-            <div
-              className='logout'
-              onClick={() => {
-                props.history.push('/login');
-              }}
-            >
-              Logout
-            </div>
-          ) : (
-            <Link to='/login' className='login'>
-              Login
-            </Link>
-          )}
-        </div>
+        <Link to='/' className='logout-button'>
+          Logout
+        </Link>
+        <div className='logged-in-display'>{uname}</div>
       </div>
       <div className='home-content-wrapper'>
-        <ul></ul>
+        <table>
+          <tr>
+            <th>Bill Name</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Due Date</th>
+            <th>Delete</th>
+          </tr>
+          {bills.map(bill => (
+            <tr>
+              <td>{bill.billname}</td>
+              <td>{bill.category ? `${bill.category}` : 'None'}</td>
+              <td>${bill.amount}</td>
+              <td>
+                {bill.duedate}
+                {bill.duedate[0] === 1 || bill.duedate[1] === 1
+                  ? 'st'
+                  : bill.duedate[0] === 2 || bill.duedate[1] === 2
+                  ? 'nd'
+                  : bill.duedate[0] === 3 || bill.duedate[1] === 3
+                  ? 'rd'
+                  : 'th'}
+              </td>
+            </tr>
+          ))}
+        </table>
       </div>
     </div>
   );
