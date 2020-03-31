45b240b66cbd6b36c9b73f3d18e0233c444ceaf9
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-03-31 12:36:51 -0400

    added proxy line to app

--- server/app.js
+++ server/app.js
@@ -19,6 +19,8 @@ app.use(
 
 app.use(express.static('../public'));
 
+app.set('trust proxy', 1);
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
