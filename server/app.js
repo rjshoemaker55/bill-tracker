34650405c007c03cb938851c947308d203beffb4
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-03-31 11:42:35 -0400

    fixed dirname

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
